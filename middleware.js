3b76a75b3a59c7607c733d19b1bace0130f32f76
Nitish <Nitishx13@gmail.com>
2025-08-29 21:24:36 +0530

    aa
    aa

--- middleware.js
+++ middleware.js
@@ -1,17 +1,20 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, createRouteMatcher } from "@clerk/nextjs";
+
+const isLoginPage = createRouteMatcher(['/login(.*)']);
+const isRegisterPage = createRouteMatcher(['/register(.*)']);
 
 export default authMiddleware({
   publicRoutes: [
     '/',
     '/home',
-    '/login',
-    '/register',
+    '/login(.*)',
+    '/register(.*)',
     '/api/(.*)'
   ],
   afterAuth(auth, req, evt) {
     // Redirect signed-in users away from login/register
     const url = new URL(req.nextUrl.origin + req.nextUrl.pathname);
-    if (auth.userId && (url.pathname.startsWith('/login') || url.pathname.startsWith('/register'))) {
+    if (auth.userId && (isLoginPage(req) || isRegisterPage(req))) {
       url.pathname = '/dashboard';
       return Response.redirect(url);
     }
